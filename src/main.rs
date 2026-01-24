0d3c842928029872bf8199e64ab7c8854d7627d1
Denis Drakhnia <numas13@gmail.com>
2026-01-24 10:39:45 +0200

    add error type

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,73 @@
-use std::cmp::min;
-use std::io::ErrorKind;
-use std::net::SocketAddr;
-use std::net::SocketAddrV4;
-use std::net::UdpSocket;
-use std::str;
-use std::thread::sleep;
-use std::time::Duration;
+use std::{
+    cmp::min,
+    io::{self, ErrorKind},
+    net::{SocketAddr, SocketAddrV4, UdpSocket},
+    process,
+    str::{self, FromStr, SplitAsciiWhitespace, Utf8Error},
+    thread::sleep,
+    time::Duration,
+};
 
-use steamworks::SteamId;
-use steamworks::{Client, User};
+use steamworks::{Client, SteamId, User};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum BrokerError {
+    #[error("send error, {0}")]
+    Send(io::Error),
+    #[error("invalid string, {0}")]
+    Utf8(#[from] Utf8Error),
+    #[error("invalid args, expect \"{0}\"")]
+    Expect(&'static str),
+    #[error("invalid args, missing \"{0}\"")]
+    Missing(&'static str),
+    #[error("failed to parse \"{0}\"")]
+    Parse(&'static str),
+}
 
 // TODO:
 // 1. Move steam iniitialization to sb_connect handler
 // 2. ... and subsequently stop steam in sb_terminate
 // 3. rename sb_terminate for something 10 byte size like sb_connect :)
 
-fn handle_connect(user: &User, buf: &[u8], from: SocketAddr, sock: &UdpSocket) {
+fn expect(args: &mut SplitAsciiWhitespace, expect: &'static str) -> Result<(), BrokerError> {
+    if args.next().ok_or(BrokerError::Missing(expect))? == expect {
+        Ok(())
+    } else {
+        Err(BrokerError::Expect(expect))
+    }
+}
+
+fn parse_next<T>(args: &mut SplitAsciiWhitespace, msg: &'static str) -> Result<T, BrokerError>
+where
+    T: FromStr,
+{
+    args.next()
+        .ok_or(BrokerError::Missing(msg))?
+        .parse()
+        .map_err(|_| BrokerError::Parse(msg))
+}
+
+fn handle_connect(
+    user: &User,
+    buf: &[u8],
+    from: SocketAddr,
+    sock: &UdpSocket,
+) -> Result<(), BrokerError> {
+    let args = str::from_utf8(buf)?;
+    println!("handle_connect: {args}");
+
     // sb_connect <ip:port> <server's steam id> <secure> <challenge>
+    let args = &mut args.split_ascii_whitespace();
+    expect(args, "sb_connect")?;
+    let serveradr: SocketAddrV4 = parse_next(args, "ip addr")?;
+    let game_server_steam_id: u64 = parse_next(args, "steam id")?;
+    let secure: bool = parse_next(args, "secure")?;
+    let challenge: i32 = parse_next(args, "challenge")?;
 
-    let Ok(x) = str::from_utf8(buf) else {
-        return;
-    };
-
-    println!("handle_connect: {x}");
-
-    let args: Vec<&str> = x.split(" ").collect();
-
-    // bruh refactor that
-    let Ok(serveradr) = args[1].parse::<SocketAddrV4>() else {
-        println!("can't parse ip addr");
-        return;
-    };
-
-    let Ok(game_server_steam_id) = args[2].parse().map(SteamId::from_raw) else {
-        println!("can't parse steam id");
-        return;
-    };
-
-    let Ok(secure) = args[3].parse::<bool>() else {
-        println!("can't parse secure");
-        return;
-    };
-
-    let Ok(challenge) = args[4].parse::<i32>() else {
-        println!("can't parse challenge");
-        return;
-    };
-
-    println!(
-        "initiate_game_connection: {serveradr} {:?} {secure} {challenge}",
-        game_server_steam_id.raw()
-    );
+    println!("initiate_game_connection: {serveradr} {game_server_steam_id} {secure} {challenge}");
     let ticket = user.initiate_game_connection(
-        game_server_steam_id,
+        SteamId::from_raw(game_server_steam_id),
         serveradr.ip().to_bits(),
         serveradr.port(),
         secure,
@@ -69,32 +84,23 @@ fn handle_connect(user: &User, buf: &[u8], from: SocketAddr, sock: &UdpSocket) {
     response.extend((ticket.len() as u32).to_le_bytes());
     response.extend(ticket);
 
-    if let Err(e) = sock.send_to(&response, from) {
-        println!("error sending: {e}");
-    }
+    sock.send_to(&response, from).map_err(BrokerError::Send)?;
+    Ok(())
 }
 
-fn handle_terminate(user: &User, buf: &[u8]) {
+fn handle_terminate(user: &User, buf: &[u8]) -> Result<(), BrokerError> {
+    let args = str::from_utf8(buf)?;
+
     // sb_terminate <ip:port> <challenge>
-    let Ok(x) = str::from_utf8(buf) else {
-        return;
-    };
-
-    let args: Vec<&str> = x.split(" ").collect();
-
-    // bruh refactor that
-    let Ok(serveradr) = args[1].parse::<SocketAddrV4>() else {
-        // TODO: return an error?
-        return;
-    };
+    let args = &mut args.split_ascii_whitespace();
+    expect(args, "sb_terminate")?;
+    let serveradr: SocketAddrV4 = parse_next(args, "ip addr")?;
+    let _challenge: i32 = parse_next(args, "challenge")?;
 
     // TODO: validate server challenge
-    let Ok(_challenge) = args[2].parse::<i32>() else {
-        // TODO: return an error?
-        return;
-    };
 
     user.terminate_game_connection(serveradr.ip().to_bits(), serveradr.port());
+    Ok(())
 }
 
 fn main() {
@@ -117,7 +123,7 @@ fn main() {
         Ok(x) => x,
         Err(e) => {
             println!("Error creating socket: {:?}", e);
-            std::process::exit(1);
+            process::exit(1);
         }
     };
     println!("Started UDP server at {addr}");
@@ -136,10 +142,14 @@ fn main() {
 
         if buf.starts_with(b"sb_connect") {
             println!("got sb_connect");
-            handle_connect(&user, &buf[..n], from, &sock);
+            if let Err(err) = handle_connect(&user, &buf[..n], from, &sock) {
+                println!("error: {err}");
+            }
         } else if buf.starts_with(b"sb_terminate") {
             println!("got sb_terminate");
-            handle_terminate(&user, &buf[..n]);
+            if let Err(err) = handle_terminate(&user, &buf[..n]) {
+                println!("error: {err}");
+            }
         } else {
             println!("Unknown packet: {:?}", &buf[0..min(n, 10)]);
         }
